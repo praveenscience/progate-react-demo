e10111f57e742579860518b6f0b46b78cd90ad13
Praveen Kumar <praveenscience@gmail.com>
2021-01-30 12:30:47 +0000

    Cleaned up the code from CRA.
    * Organised the code into right places.
    * Removed unnecessary assets.

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello Progate</h1>
+        <h2>Students are Awesome!</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
