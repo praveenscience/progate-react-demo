73ef56ac40212b255f676013e00291aec4644d4e
Praveen Kumar <praveenscience@gmail.com>
2021-01-30 12:53:16 +0000

    Implemented a connected component using React.

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,34 @@
 import React, { Component } from "react";
 import Header from "./Header";
 class App extends Component {
+  state = {
+    Dark: true
+  };
+  handleCheck = e => {
+    this.setState({ Dark: e.target.checked });
+  };
   render() {
     return (
       <div className="App">
-        {[
-          "Progate App",
-          "Progate App Lite",
-          "Progate App Dark",
-          "Light Weight",
-          "Dark Chocolate"
-        ].map((text, key) => (
-          <Header dark={key % 2 === 0} key={key}>
-            {text}
-          </Header>
-        ))}
+        <Header dark={this.state.Dark}>Progate App</Header>
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <div className="form-check p-5">
+                <input
+                  type="checkbox"
+                  id="frmCheck"
+                  className="form-check-input mr-2"
+                  checked={this.state.Dark}
+                  onChange={this.handleCheck}
+                />
+                <label htmlFor="frmCheck" className="form-check-label">
+                  Dark
+                </label>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
